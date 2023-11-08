55bb6d9dec0e9055a858399e69d21bdf9b3af565
Niclas-Thms <148530610+Niclas-Thms@users.noreply.github.com>
2023-11-08 19:59:07 +0100

    Add files via upload

---
+++ PART III/main.cpp
@@ -0,0 +1,40 @@
+#include "Option.h"
+#include "VanillaOption.h"
+#include "CallOption.h"  // Include the CallOption header
+#include "PutOption.h"   // Include the PutOption header
+#include "AmericanOption.h"
+#include "AmericanCallOption.h"
+#include "AmericanPutOption.h"
+#include "DigitalOption.h"
+#include "DigitalCallOption.h"
+#include "DigitalPutOption.h"
+#include "AsianOption.h"
+#include "AsianCallOption.h"
+#include "AsianPutOption.h"
+#include "BlackScholesPricer.h"
+#include <iostream>
+
+
+int main() {
+    // Create instances of derived classes, not the base class
+    CallOption callOption(1.0, 100.0);
+    PutOption putOption(1.0, 100.0);
+    DigitalCallOption digit(1.0, 100.0);
+    BlackScholesPricer pricer(&callOption, 105.0, 0.05, 0.2);
+    BlackScholesPricer pricerd(&digit, 105.0, 0.05, 0.2);
+
+    AmericanCallOption(1.0, 100);
+
+    double optionPrice = pricer();
+    double optionDelta = pricer.delta();
+    double optiondigit = pricerd();
+
+
+    std::cout << "Call Option Price: " << optionPrice << std::endl;
+    std::cout << "Call Option Delta: " << optionDelta << std::endl;
+    std::cout << "Call digit Price: " << optiondigit << std::endl;
+
+
+
+    return 0;
+}
