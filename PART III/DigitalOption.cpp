28dd900ec410950a00593b52e49c225408984e53
Loreuxe <67767260+Loreuxe@users.noreply.github.com>
2023-11-22 17:43:15 +0100

    fPART II BIS

--- PART III/DigitalOption.cpp
+++ PART III/DigitalOption.cpp
@@ -1,11 +1,12 @@
 #include "DigitalOption.h"
 
+
 OptionNature DigitalOption::GetOptionNature() const { return OptionNature::Digital; }
 
 
 DigitalOption::DigitalOption(double expiry, double strike) : Option(expiry), _strike(strike) {
 
-    if (_strike < 0.0) { throw std::invalid_argument::invalid_argument("Strike must be non negative"); }
+    if (_strike < 0.0) { throw std::invalid_argument("Strike must be non negative"); }
 
 }
 
